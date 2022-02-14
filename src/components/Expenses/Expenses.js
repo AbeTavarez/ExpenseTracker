a15286e746f03e0a4ca3bb7ec0f10cbcc2699a11
Abraham E. Tavarez <abetavarez@icloud.com>
2022-02-13 23:51:02 -0500

    filter component for expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,35 @@
+import { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 
-const Expenses = ({items}) => {
+const Expenses = ({ items }) => {
+  const [selectedYear, setSelectedYear] = useState('2020');
+  console.log(selectedYear);
+  const handleFilterChange = (selectedYear) => setSelectedYear(selectedYear);
+
   return (
     <div>
-      <ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date}/>
-      <ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date}/>
-      <ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date}/>
+      <ExpensesFilter
+        selected={selectedYear}
+        setSelectedYear={handleFilterChange}
+      />
+      <ExpenseItem
+        title={items[0].title}
+        amount={items[0].amount}
+        date={items[0].date}
+      />
+      <ExpenseItem
+        title={items[1].title}
+        amount={items[1].amount}
+        date={items[1].date}
+      />
+      <ExpenseItem
+        title={items[2].title}
+        amount={items[2].amount}
+        date={items[2].date}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Expenses;
---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const ExpensesFilter = (props) => {
+  const handleChange = (e) => props.setSelectedYear(e.target.value);
+
+  return (
+    <div>
+      <div>
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={handleChange}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
